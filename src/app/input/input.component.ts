01153c4b39e9a66acf40dfd461fa1c8789f67e54
Matthew Luedtke <MLuedtke@tql.com>
2022-02-21 16:32:28 -0500

    updated UI

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -4,16 +4,14 @@ import { AppComponent } from '../app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
-
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss']
 })
+
 export class InputComponent implements OnInit {
 
-  
-  
   constructor(
     private httpClient: HttpClient
   ) { }
@@ -36,23 +34,24 @@ export class InputComponent implements OnInit {
   }
 
   submit(): void {
-
+    this.cLetter = this.centerLetter;
+    this.letts = this.letters;
+    this.invalidLetters = ['/', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '-', '.', '1', '0', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    this.lettersArray = [];
+    this.acceptableWords= [];
     this.letters.split('').forEach(letter=> {
       this.removeLetter(letter.toLowerCase());
     });
     this.removeLetter(this.centerLetter.toLowerCase())
     console.log(this.invalidLetters);
     this.solve(this.centerLetter.toLowerCase(), this.invalidLetters);
-    this.invalidLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '-', '.', '1', '0', '2', '3', '4', '5', '6', '7', '8', '9'];
-    this.centerLetter = "";
-    this.letters = "";
-    this.lettersArray = [];
-    this.acceptableWords= [];
   }
 
   removeLetter(letter: string): void{
     var i = this.invalidLetters.indexOf(letter);
+    if(i>-1){
     this.invalidLetters.splice(i, 1);
+    }
   }
 
   acceptable = true;
@@ -75,6 +74,14 @@ export class InputComponent implements OnInit {
       this.acceptable = true;  
     });
     console.log(this.acceptableWords);
+  }
+  isThereData(): boolean {
+    if(this.cLetter && this.letts){
+      return true;
+    }
+    else {
+      return false;
+    }
   }
   
 
